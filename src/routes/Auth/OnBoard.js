5856c041bb43c381a197ca870ab29f57b0317e26
Suthan PC <suthan0015@gmail.com>
2022-07-22 23:23:20 +0530

    react native link asset

--- src/routes/Auth/OnBoard.js
+++ src/routes/Auth/OnBoard.js
@@ -1,19 +1,19 @@
-import {Text, Image, Center, Button} from 'native-base';
+import { Text, Image, Center, Button } from 'native-base';
 import React from 'react';
-import {useNavigation} from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 import GButton from '../../comp/GButton';
 
 function OnBoard() {
   const navigation = useNavigation();
 
   return (
-    <Center justifyContent="center" flex={1}>
+    <Center justifyContent="center" flex={1} bg="white">
       <Image
         source={require('../../asset/img/logo.png')}
         alt="Marketplace img"
         size="3xs"
         resizeMode="contain"
-        mb={-16}
+        mb={5}
       />
       <GButton label="Login" onPress={() => navigation.navigate('Login')} />
 
